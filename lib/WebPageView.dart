9e58e0b01f2662eb01579fd7df7400da4eb4990d
dramikei <raghav2011rv@gmail.com>
2020-01-31 21:49:21 +0530

    Added back button

--- lib/WebPageView.dart
+++ lib/WebPageView.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
-class WebPageView extends StatelessWidget{
+class WebPageView extends StatefulWidget{
   @override
-  Widget build(BuildContext context)
-  {
+  State<StatefulWidget> createState(){
+    return WebPageViewState();
+  }
+}
+class WebPageViewState extends State<WebPageView>{
 
+  Widget build(BuildContext context)
+  { 
     return (new WebviewScaffold(
           url: "https://192.168.24.47:3000",
           withJavascript: true,
           appBar: new AppBar(
-            title: new Text("Widget webview"),
+            title: new Text("PlanIT!"),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: ()
+              {
+                // Navigator.pop(context);
+                Navigator.push(context, MaterialPageRoute(builder:(context)=>WebPageView()));
+              },
+            ),
           ),
           ignoreSSLErrors: true,
           geolocationEnabled: true,
-          debuggingEnabled: true
-        )
-    );
+        ));
   }
 }
